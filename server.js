ccd5d6d595d99e570a42ccb29dd08eab38d87d2b
Bruno Oliveira <bruno.oliveira@climatempo.com>
2019-12-04 09:38:08 -0200

    Create server to render html

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+
+app.use('/static', express.static(__dirname + '/public/'));
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/src/index.html');
-    });
+  res.sendfile(__dirname + '/src/index.html');
+});
+    
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
