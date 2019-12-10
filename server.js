d40f764d69b6c553b3a4e52c739a11e1b5160a0d
Bruno Oliveira <bruno.oliveira@climatempo.com>
2019-12-10 21:47:48 -0200

    fixed express server to read absolute path

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.use('/static', express.static(__dirname + '/public/'));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/src/index.html');
 });
+
+app.get('/naoobfuscado', function (req, res) {
+  res.sendfile(__dirname + '/src/notObfuscate.html');
+});
+
+app.get('/indexjs', function (req, res) {
+  res.sendfile(__dirname + '/src/index.js');
+});
     
 
 app.listen(3000, function () {
